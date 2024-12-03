d112321bda9da30f70331b20326e1cad68c459f2
Jean Sanchuki <jean.sanchuki@gmail.com>
2024-12-03 19:35:28 -0300

    feat: added API server

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import accountRoutes from './routes/accountRoutes';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use('/accounts', accountRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
